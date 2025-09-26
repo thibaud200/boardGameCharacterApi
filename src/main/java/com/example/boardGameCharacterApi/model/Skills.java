a507d035b950b3da1542c7bf99a85cfe58ac99b7
Thibs200 <144443234+thibaud200@users.noreply.github.com>
2025-09-26 21:42:20 +0200

    Update Skills class to enforce name non-nullable
    Added nullable constraint to level field.

--- src/main/java/com/example/boardGameCharacterApi/model/Skills.java
+++ src/main/java/com/example/boardGameCharacterApi/model/Skills.java
@@ -13,12 +13,14 @@ public class Skills {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = true)
     private Integer level;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "character_id")
     private Character character;
 }
+
 
