3b24e3f111359a551e4880c9c1008a5eef39bace
Thibs200 <144443234+thibaud200@users.noreply.github.com>
2025-09-26 21:41:24 +0200

    Refactor CharacterType entity fields and table name
    Updated the CharacterType entity to improve field constraints and table naming.

--- src/main/java/com/example/boardGameCharacterApi/model/CharacterType.java
+++ src/main/java/com/example/boardGameCharacterApi/model/CharacterType.java
@@ -11,19 +11,20 @@ import org.springframework.context.annotation.Description;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "charactersType")       
+@Table(name = "character_types")       
 public class CharacterType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Name
+    @Column(nullable = false, unique = true)
     private String name;
-    @Description
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @ManyToMany(mappedBy = "types")
     private Set<Character> characters;
 }
+
 
 
